ee127c34b3983dc2b2dcdaebfe5c1b7ba1c76f9a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-01-14 12:15:47 +0900

    Support modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,31 @@
+import commonjs from 'rollup-plugin-commonjs';
+import env from 'rollup-plugin-env';
+import globals from 'rollup-plugin-node-globals';
 import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace'
 import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/main.js',
   output: {
-    file: 'bundle.js',
-    format: 'cjs'
+    file: 'dist/bundle.js',
+    format: 'iife'
   },
   plugins: [
-    resolve(),
-    babel(),
+    babel({
+      "exclude": "node_modules/**",
+    }),
+    // env({
+    //   NODE_ENV: process.env.NODE_ENV,
+    // }),
+    commonjs(),
+    globals(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+    }),
   ]
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+import messages from './messages';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   console.log(`Start app at ${new Date()}.`);
+//
+//   const applicationMainElement = window.document.querySelector('.application');
+//
+//   ReactDOM.render(
+//     <h1>Hello Rollup</h1>,
+//     applicationMainElement,
+//   );
+// });
+
 window.addEventListener('DOMContentLoaded', () => {
   console.log(`Start app at ${new Date()}.`);
+  console.log(messages.start);
 });
