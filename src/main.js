75cc0b4c7ff81c52813ab25ae8a44d9fd6ebb917
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-01-14 12:17:47 +0900

    Enable jsx

--- src/main.js
+++ src/main.js
@@ -2,18 +2,15 @@ import messages from './messages';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// window.addEventListener('DOMContentLoaded', () => {
-//   console.log(`Start app at ${new Date()}.`);
-//
-//   const applicationMainElement = window.document.querySelector('.application');
-//
-//   ReactDOM.render(
-//     <h1>Hello Rollup</h1>,
-//     applicationMainElement,
-//   );
-// });
-
 window.addEventListener('DOMContentLoaded', () => {
   console.log(`Start app at ${new Date()}.`);
   console.log(messages.start);
+  const applicationMainElement = window.document.querySelector('.application');
+
+  if (applicationMainElement !== null) {
+    ReactDOM.render(
+      <h1>Hello Rollup</h1>,
+      applicationMainElement,
+    );
+  }
 });
